732bb91e1b4067a5a10bd8b31a9d592f42351b50
Dokman <chaoticanthrim@hotmail.com>
2024-09-17 17:52:55 +0200

    Load before launch (Not yet usable)

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,16 +1,23 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Material.Colors;
+using Material.Styles.Themes;
+using Material.Styles.Themes.Base;
 using PackwizModpackManager.ViewModels;
 using PackwizModpackManager.Views;
+using System.Linq;
 
 namespace PackwizModpackManager
 {
     public partial class App : Application
     {
+        private MaterialTheme _materialTheme;
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+            _materialTheme = this.Styles.OfType<MaterialTheme>().FirstOrDefault();
         }
 
         public override void OnFrameworkInitializationCompleted()
@@ -24,6 +31,17 @@ namespace PackwizModpackManager
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        // Método para cambiar el tema y los colores
+        public void SetTheme(BaseThemeMode baseTheme, PrimaryColor primaryColor, SecondaryColor secondaryColor)
+        {
+            if (_materialTheme != null)
+            {
+                _materialTheme.BaseTheme = baseTheme;
+                _materialTheme.PrimaryColor = primaryColor;
+                _materialTheme.SecondaryColor = secondaryColor;
+            }
         }
     }
 }
