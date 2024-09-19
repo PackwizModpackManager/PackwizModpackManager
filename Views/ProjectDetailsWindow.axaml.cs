ae32cebbefbe1ad8d4ed0b6d021d92ada1e2694a
Dokman <chaoticanthrim@hotmail.com>
2024-09-19 08:51:53 +0200

    Should not ask so time to overwirte to automatically accept

--- Views/ProjectDetailsWindow.axaml.cs
+++ Views/ProjectDetailsWindow.axaml.cs
@@ -186,7 +186,7 @@ namespace PackwizModpackManager.Views
         private async void OnUpdateAllModsClick(object sender, RoutedEventArgs e)
         {
             // Construir los argumentos del comando para actualizar todos los mods
-            string arguments = "update --all";
+            string arguments = "update --all -y";
 
             // Ejecutar el comando de Packwiz para actualizar todos los mods
             await ExecutePackwizCommand(arguments);
